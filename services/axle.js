6cb694ca2db73db65c73f34f750941d24d66ed59
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2017-12-22 00:46:14 -0800

    Stab at a new axle

--- services/axle.js
+++ services/axle.js
@@ -1,62 +1,116 @@
+'use strict';
+
 console.info('Loading Axle.');
+
+const ROSAPIAngle = require('services/ros-api-angle');
+const ROSAPIVelocity = require('services/ros-api-velocity');
 
 function axle(config)
 {
-  this._name = config.name;
-  this._wheels = config.wheels;
-  this._perRadian = config.time360Ms / (Math.PI * 2);
+  this._node = rosNode.init(config.name);
+  this._left = config.left;
+  this._right = config.right;
+  this._steering = config.steering;
+  this._lastVelocity = 0;
+  this._rosApiAngle = new ROSAPIAngle(this);
+  this._rosApiVelocity = new ROSAPIVelocity(this);
 }
 
-axle.prototype =
+axle.prototype = 
 {
   enable: function()
   {
-    this._wheels.forEach(function(wheel)
-    {
-      wheel.servo.enable();
-    });
+    this._left.enable();
+    this._right.enable();
+    this._steering && this._steering.enable();
+    this._rosApiAngle.enable();
+    this._rosApiVelocity.enable();
     return this;
   },
   
   disable: function()
   {
-    this._wheels.forEach(function(wheel)
-    {
-      wheel.servo.disable();
-    });
+    this._rosApiVelocity.disable();
+    this._rosApiAngle.disable();
+    this._steering && this._steering.disable();
+    this._right.disable();
+    this._left.disable();
     return this;
   },
   
-  setDirection(direction, timeMs)
+  setVelocity(velocity, changeMs, func)
   {
-    this._wheels.forEach(function(wheel)
-    {
-      wheel.servo.setDirection(wheel.direction * direction, timeMs);
-    });
+    this._left.setVelocity(velocity, changeMs, func);
+    this._right.setVelocity(velocity, changeMs, func);
+    this._lastVelocity = velocity;
   },
-  
-  turn: function(angleRadians)
+
+  getCurrentVelocity: function()
   {
-    if (angleRadians !== 0)
+    return (this._left.getCurrentVelocity() + this._right.getCurrentVelocity()) / 2;
+  },
+
+  brake: function()
+  {
+    this._left.brake();
+    this._right.brake();
+    this._lastVelocity = 0;
+  },
+
+  idle: function(idle)
+  {
+    this._left.idle(idle);
+    this._right.idle(idle);
+    this._steering && this._steering.idle(idle);
+  },
+
+  isVelocityChanging: function()
+  {
+    return this._left.isVelocityChanging() || this._right.isVelocityChanging();
+  },
+
+  setAngle: function(angleRadians, changeMs, func)
+  {
+    if (this._steering)
     {
-      var direction = angleRadians < 0 ? -1 : 1;
-      var timeMs = direction * angleRadians * this._perRadian;
-      this._wheels.forEach(function(wheel)
+      this._steering.setAngle(angleRadians, changeMs, func);
+    }
+    else
+    {
+      let c = Math.sin(angleRadians);
+      this._left.setVelocity(this._lastVelocity * (1 + c), changeMs, func);
+      this._right.setVelocity(this._lastVelocity * (1 - c), changeMs, func);
+    }
+  },
+
+  getCurrentAngle: function()
+  {
+    if (this._steering)
+    {
+      return this._steering.getCurrentVelocity();
+    }
+    else
+    {
+      let left = this._left.getCurrentVelocity();
+      let right = this._right.getCurrentVelocity();
+      let selected = right;
+      if (Math.abs(left) < Math.abs(right))
       {
-        switch (wheel.side)
-        {
-          case 'left':
-            wheel.servo.setDirection(direction * wheel.direction, timeMs);
-            break;
-            
-          case 'right':
-            wheel.servo.setDirection(-direction * wheel.direction, timeMs);
-            break;
-            
-          default:
-            break;
-        }
-      });
+        selected = left;
+      }
+      return this._lastVelocity / selected;
+    }
+  },
+
+  isAngleChanging: function()
+  {
+    if (this._steering)
+    {
+      return this._steering.isAngleChanging();
+    }
+    else
+    {
+      return false; // Unknown
     }
   }
 };
