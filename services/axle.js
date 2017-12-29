dd1c8f5a553a081a1c64ab9e0446b349d9036935
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2017-12-29 00:12:50 -0500

    Axle handles two configurations
    1. Tank mode (two separate driving wheels)
    2. Steering mode (one motor, one servo turning the axle)

--- services/axle.js
+++ services/axle.js
@@ -7,9 +7,11 @@ const ROSAPIVelocity = require('services/ros-api-velocity');
 
 function axle(config)
 {
+  this._name = config.name;
   this._node = rosNode.init(config.name);
   this._left = config.left;
   this._right = config.right;
+  this._drive = config.drive;
   this._steering = config.steering;
   this._lastVelocity = 0;
   this._rosApiAngle = new ROSAPIAngle(this);
@@ -20,8 +22,9 @@ axle.prototype =
 {
   enable: function()
   {
-    this._left.enable();
-    this._right.enable();
+    this._left && this._left.enable();
+    this._right && this._right.enable();
+    this._drive && this._drive.enable();
     this._steering && this._steering.enable();
     this._rosApiAngle.enable();
     this._rosApiVelocity.enable();
@@ -33,40 +36,76 @@ axle.prototype =
     this._rosApiVelocity.disable();
     this._rosApiAngle.disable();
     this._steering && this._steering.disable();
-    this._right.disable();
-    this._left.disable();
+    this._drive && this._drive.disable();
+    this._right && this._right.disable();
+    this._left && this._left.disable();
     return this;
   },
   
   setVelocity(velocity, changeMs, func)
   {
-    this._left.setVelocity(velocity, changeMs, func);
-    this._right.setVelocity(velocity, changeMs, func);
+    if (this._drive)
+    {
+      this._drive.setVelocity(velocity, changeMs, func);
+    }
+    else
+    {
+      this._left.setVelocity(velocity, changeMs, func);
+      this._right.setVelocity(velocity, changeMs, func);
+    }
     this._lastVelocity = velocity;
   },
 
   getCurrentVelocity: function()
   {
-    return (this._left.getCurrentVelocity() + this._right.getCurrentVelocity()) / 2;
+    if (this._drive)
+    {
+      return this._drive.getCurrentVelocity();
+    }
+    else
+    {
+      return (this._left.getCurrentVelocity() + this._right.getCurrentVelocity()) / 2;
+    }
   },
 
   brake: function()
   {
-    this._left.brake();
-    this._right.brake();
+    if (this._drive)
+    {
+      this._drive.brake();
+    }
+    else
+    {
+      this._left.brake();
+      this._right.brake();
+    }
     this._lastVelocity = 0;
   },
 
   idle: function(idle)
   {
-    this._left.idle(idle);
-    this._right.idle(idle);
-    this._steering && this._steering.idle(idle);
+    if (this._drive)
+    {
+      this._drive.idle(idle);
+      this._steering.idle(idle);
+    }
+    else
+    {
+      this._left.idle(idle);
+      this._right.idle(idle);
+    }
   },
 
   isVelocityChanging: function()
   {
-    return this._left.isVelocityChanging() || this._right.isVelocityChanging();
+    if (this._drive)
+    {
+      return this._drive.isVelocityChanging();
+    }
+    else
+    {
+      return this._left.isVelocityChanging() || this._right.isVelocityChanging();
+    }
   },
 
   setAngle: function(angleRadians, changeMs, func)
