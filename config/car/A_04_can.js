afd30d1aa336b7b8c5e5f01510c839cb90ebc761
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-01-04 22:38:04 -0800

    Use gpio 5 for can interrupt

--- config/car/A_04_can.js
+++ config/car/A_04_can.js
@@ -4,11 +4,8 @@ const CAN = require('hw/can/can-mcp2515');
 global.CAN = new CAN(
 {
   spi: SPI.open(),
-  speed: 500, // 500kbs
-  interrupt: GPIO.open(
-  {
-    channel: 0
-  })
+  speed: 500, // kbs
+  interrupt: GPIO.open({ channel: 5 })
 });
 
 module.exports = function() {}
