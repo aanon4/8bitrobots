e7e5bb0dbee99c4b2f373d129db38f3ec555090a
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-01-15 23:45:58 -0800

    Tweak sending frequency

--- services/vision/RobotVision/main.cpp
+++ services/vision/RobotVision/main.cpp
@@ -1,32 +1,54 @@
 #include "video_server.hpp"
+#include "lane_detector.hpp"
 
 using namespace cv;
 using namespace std;
 
+const float FRAME_DELAY = 8; // I'm seeing an 8-frame delay to render
+const float MAX_LATENCY = 0.100; // 100ms is the acceptable latency
+const float FRAME_RATE = (int)(FRAME_DELAY / MAX_LATENCY); // So this is the required frame rate
+const chrono::milliseconds frame_time((int)(1000.0 / FRAME_RATE));
+
 int main()
 {
-  VideoServer video;
+  VideoServer server;
+  LaneDetector lane;
 
-  thread video_thread([&video]() {
-    video.run();
+  thread server_thread([&server]() {
+    server.run();
   });
 
-  thread capture_thread([&video]() {
+  thread capture_thread([&server, &lane]() {
     VideoCapture cap;
-    cap.open(0 + cv::CAP_ANY);
+    cap.open(0, CAP_ANY);
+    cap.set(CAP_PROP_FRAME_WIDTH, 320);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 240);
+    cap.set(CAP_PROP_FPS, FRAME_RATE);
     if (!cap.isOpened()) {
       cerr << "Error";
       return -1;
-    } 
-    Mat frame;
+    }
+
     for (;;) {
+      auto start = chrono::steady_clock::now();
+
+      Mat frame;
       cap.read(frame);
-      video.set_image(frame);
-//      this_thread::sleep_for(chrono::milliseconds((int)(1000 / 20)));
+
+      //lane.process_image(frame);
+      //lane.get_result(&frame);
+
+      // Send frame off to be viewed
+      server.set_image(frame);
+
+      auto duration = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start);
+      if (duration < frame_time) {
+        this_thread::sleep_for(frame_time - duration);
+      }
     }
   });
 
 
-  video_thread.join();
+  server_thread.join();
   capture_thread.join();
 }
