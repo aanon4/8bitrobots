c76a9657bb9fcbaedf2a1111cf11718ffe5185b0
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-01-15 23:46:53 -0800

    Startup the IMU

---
+++ config/robot/C_02_imu.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function()
+{
+  const i2cMPU9250 = I2C[0].open({ address: 0x68 });
+  if (i2cMPU9250.valid())
+  {
+    const Imu = require('hw/imu/imu-mpu9250');
+    return new Imu(
+    {
+      name: '/imu/monitor', 
+      reset: false,
+      i2c:
+      {
+        MPU9250: i2cMPU9250,
+        AK8963: I2C[0].open({ address: 0x0C }),
+      }
+    });
+  }
+}
