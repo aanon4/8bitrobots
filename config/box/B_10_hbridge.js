7f23314159c471d72fdd19dea5961978c0deea03
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-02-16 22:15:33 -0800

    Basic hbridge setup

---
+++ config/box/B_10_hbridge.js
@@ -0,0 +1,12 @@
+const HBridge = require('hw/hbridge/hbridge-drv8833');
+
+global.HBRIDGE = new HBridge(
+{
+  v: 5.0,
+  ain1: PWM.open({ channel: 15 }),
+  ain2: PWM.open({ channel: 14 }),
+  bin1: PWM.open({ channel: 13 }),
+  bin2: PWM.open({ channel: 12 })
+});
+
+module.exports = function() {}
