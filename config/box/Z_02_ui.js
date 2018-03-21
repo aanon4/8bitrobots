e394259bbe1fbe79da22feb10b149b741eaa237f
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-03-20 22:52:05 -0700

    Blockly import

--- config/box/Z_02_ui.js
+++ config/box/Z_02_ui.js
@@ -10,7 +10,13 @@ module.exports = function()
       '/': 'config/box/ui/controller.html',
       '/js/8bit.js': './modules/8bit.js',
       '/js/8bit-webconnector.js': './modules/8bit-webconnector.js',
-      '/image/compass.png': 'config/box/ui/compass.png'
+      '/image/compass.png': 'config/box/ui/compass.png',
+
+      // Blockly
+      '/blockly/blockly_compressed.js': './modules//blockly/blockly_compressed.js',
+      '/blockly/blocks_compressed.js': '/modules//blockly/blockly_compressed.js',
+      '/blockly/msg/js/': './modules/msg/js/',
+      '/blockly/8bitblocks.js': './modules/blockly/8bitblocks.js'
     }
   });
 }
