e2c4f7f20bab1b8318089444ca340af104f42ac5
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-03-22 18:03:59 -0700

    Expand GPIO

--- hw/board/raspberrypi/gpio.js
+++ hw/board/raspberrypi/gpio.js
@@ -256,7 +256,19 @@ function gpios()
     new gpioChannel(this, 9),
     new gpioChannel(this, 22),
     new gpioChannel(this, 14),
-    new gpioChannel(this, 15)
+    new gpioChannel(this, 15),
+
+    new gpioChannel(this, 5),
+    new gpioChannel(this, 6),
+    new gpioChannel(this, 13),
+    new gpioChannel(this, 19),
+    new gpioChannel(this, 26),
+    new gpioChannel(this, 12),
+    new gpioChannel(this, 16),
+    new gpioChannel(this, 20),
+    new gpioChannel(this, 21),
+    new gpioChannel(this, 0),
+    new gpioChannel(this, 1)
   ]
   this._channels =
   {
@@ -277,24 +289,44 @@ function gpios()
     WPI14: gpios[14],
     WPI15: gpios[15],
     WPI16: gpios[16],
+    WPI21: gpios[17],
+    WPI22: gpios[18],
+    WPI23: gpios[19],
+    WPI24: gpios[20],
+    WPI25: gpios[21],
+    WPI26: gpios[22],
+    WPI27: gpios[23],
+    WPI28: gpios[24],
+    WPI29: gpios[25],
+    WPI30: gpios[26],
+    WPI31: gpios[27],
     
+    GPIO0: gpios[0],
+    GPIO1: gpios[1],
     GPIO2: gpios[8],
     GPIO3: gpios[9],
     GPIO4: gpios[7],
+    GPIO5: gpios[5],
+    GPIO6: gpios[6],
     GPIO7: gpios[11],
     GPIO8: gpios[10],
     GPIO9: gpios[13],
     GPIO10: gpios[12],
     GPIO11: gpios[14],
+    GPIO13: gpios[],
     GPIO14: gpios[15],
     GPIO15: gpios[16],
     GPIO17: gpios[0],
     GPIO18: gpios[1],
-    GPIO22: gpios[3],
-    GPIO23: gpios[4],
-    GOIO24: gpios[5],
-    GPIO25: gpios[6],
-    GPIO27: gpios[2]
+    GPIO21: gpios[17],
+    GPIO22: gpios[18],
+    GPIO23: gpios[19],
+    GOIO24: gpios[20],
+    GPIO25: gpios[21],
+    GPIO26: gpios[22],
+    GPIO27: gpios[23],
+    GPIO28: gpios[24],
+    GPIO29: gpios[25]
   };
 
   PIGPIO.initialize();
