5adf9c5a3f150135d9d4b22ce496081766824df0
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-03-28 16:24:21 -0700

    Protect HBRIDGE startup incase PWM isn't available

--- config/box/B_10_hbridge.js
+++ config/box/B_10_hbridge.js
@@ -1,12 +1,15 @@
 const HBridge = require('hw/hbridge/hbridge-drv8833');
 
-global.HBRIDGE = new HBridge(
+if (typeof PWM !== 'undefined')
 {
-  v: 5.0,
-  ain1: PWM.open({ channel: 15 }),
-  ain2: PWM.open({ channel: 14 }),
-  bin1: PWM.open({ channel: 13 }),
-  bin2: PWM.open({ channel: 12 })
-});
+  global.HBRIDGE = new HBridge(
+  {
+    v: 5.0,
+    ain1: PWM.open({ channel: 15 }),
+    ain2: PWM.open({ channel: 14 }),
+    bin1: PWM.open({ channel: 13 }),
+    bin2: PWM.open({ channel: 12 })
+  });
+}
 
 module.exports = function() {}
