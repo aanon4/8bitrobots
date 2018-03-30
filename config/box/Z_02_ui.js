1b67db52ade5f7dae1b1d4f12b0288e083a62c5d
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-03-29 17:32:04 -0700

    Use quaternion in UI

--- config/box/Z_02_ui.js
+++ config/box/Z_02_ui.js
@@ -10,6 +10,7 @@ module.exports = function()
       '/': 'config/box/ui/controller.html',
       '/js/8bit.js': './modules/8bit.js',
       '/js/8bit-webconnector.js': './modules/8bit-webconnector.js',
+      '/js/three.js': './node_modules/three/build/three.min.js',
       '/image/compass.png': 'config/box/ui/compass.png',
 
       // Blockly
