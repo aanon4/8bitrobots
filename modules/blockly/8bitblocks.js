457d7a1eb5ea1fc0b8976e274a18221034d7c19a
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-04-16 11:41:19 -0700

    Immediately configure hardware
    Configure hardware as changes are made, then update the topics and
    services available (which may have changed)

--- modules/blockly/8bitblocks.js
+++ modules/blockly/8bitblocks.js
@@ -1,79 +1,187 @@
-function buildProgramBlocks()
-{
-  const json0 =
+ (function()
+ {
+  function buildProgramBlocks()
   {
-    message0: 'Setup',
-    message1: 'do %1',
-    args1:
-    [
-      {
-        type: 'input_statement',
-        name: 'begin'
-      }
-    ], 
-    message2: 'then loop',
-    message3: 'do %1',
-    args3:
-    [
-      {
-        type: 'input_statement',
-        name: 'loop'
-      }
-    ],
-    colour: 120
-  };
-
-  Blockly.Blocks['main_program'] =
-  {
-    init: function()
+    const json0 =
     {
-      this.jsonInit(json0);
-    }
-  }
+      message0: 'Loop forever',
+      message1: 'do %1',
+      args1:
+      [
+        {
+          type: 'input_statement',
+          name: 'loop'
+        }
+      ],
+      colour: 120
+    };
 
-  const json1 =
-  {
-    message0: 'Wait for events',
-    previousStatement: null,
-    nextStatement: null,
-    colour: 120
-  };
-
-  Blockly.Blocks['wait_for_events'] =
-  {
-    init: function()
+    Blockly.Blocks['main_program'] =
     {
-      this.jsonInit(json1);
+      init: function()
+      {
+        this.jsonInit(json0);
+      }
     }
+
+    const json1 =
+    {
+      message0: 'Wait for events',
+      previousStatement: null,
+      nextStatement: null,
+      colour: 120
+    };
+
+    Blockly.Blocks['wait_for_events'] =
+    {
+      init: function()
+      {
+        this.jsonInit(json1);
+      }
+    }
+
+    const idx = TOOLBOX.indexOf('<category name="Program">');
+    if (idx !== -1)
+    {
+      TOOLBOX[idx + 1] = `
+        <block type="main_program"></block>
+        <block type="wait_for_events"></block>
+      `;
+    }
+    WORKSPACE.updateToolbox(TOOLBOX.join());
   }
 
-  const idx = TOOLBOX.indexOf('<category name="Program">');
-  if (idx !== -1)
+  function buildConfigBlocks(services)
   {
-    TOOLBOX[idx + 1] = `
-      <block type="main_program"></block>
-      <block type="wait_for_events"></block>
-    `;
+    const configureBlocks = [];
+    let blocks = 0;
+    services.forEach((service) => {
+
+      const name = service.name;
+      const schema = service.schema;
+
+      const CONFIG = NODE.proxy({ service: name });
+      CONFIG({}).then((config) => {
+        let json =
+        {
+          message0: `Configure ${name.substr(0, name.length - 7)}`,
+          colour: 90
+        };
+        let count = 1;
+        for (let key in schema)
+        {
+          if (key !== '__return')
+          {
+            if (typeof schema[key] === 'object')
+            {
+              let def = schema[key].indexOf(config[key]);
+              if (def !== -1)
+              {
+                schema[key].splice(def, 1);
+                schema[key].unshift(config[key]);
+              }
+              json[`args${count}`]= [
+              {
+                type: 'field_dropdown',
+                name: key,
+                check: 'String',
+                options: schema[key].map((value) => [ value, value ])
+              }];
+            }
+            else
+            {
+              switch (schema[key])
+              {
+                case 'String':
+                default:
+                  json[`args${count}`] = [
+                  {
+                    type: 'field_input',
+                    name: key,
+                    text: key in config ? config[key] : ''
+                  }];
+                  break;
+                case 'Number':
+                  json[`args${count}`] = [
+                  {
+                    type: 'field_number',
+                    name: key,
+                    value: key in config ? config[key] : 0
+                  }];
+                  break;
+                case 'Boolean':
+                  json[`args${count}`] = [
+                  {
+                    type: 'field_checkbox',
+                    name: key,
+                    checked: key in config ? !!config[key] : false
+                  }];
+                  break;
+              }
+            }
+            json[`message${count}`] = `${key} %1`;
+            count++;
+          }
+        }
+    
+        Blockly.Blocks[name] =
+        {
+          init: function()
+          {
+            this.jsonInit(json);
+          },
+
+          onchange: function(e)
+          {
+            switch (e.type)
+            {
+              case 'create':
+                break;
+
+              case 'change':
+                if (this.id !== e.blockId)
+                {
+                  break;
+                }
+                CONFIG({ [e.name]: e.newValue }).then(() => {
+                  rebuildEventAndActionBlocks();
+                });
+                break;
+              default:
+                break;
+            }
+          }
+        }
+
+        configureBlocks.push(`<block type="${name}" />`);
+
+        if (++blocks == services.length)
+        {
+          const idx = TOOLBOX.indexOf('<category name="Configure">');
+          if (idx !== -1)
+          {
+            TOOLBOX[idx + 1] = configureBlocks.join();
+          }
+          WORKSPACE.updateToolbox(TOOLBOX.join());
+        }
+      });
+    });
   }
-  WORKSPACE.updateToolbox(TOOLBOX.join());
-}
 
-function buildConfigBlocks(services)
-{
-  const configureBlocks = [];
-  let blocks = 0;
-  services.forEach((service) => {
+  function buildActionBlocks(actions)
+  {
+    const actionBlocks = [];
+    actions.forEach((action) => {
 
-    const name = service.name;
-    const schema = service.schema;
-
-    NODE.proxy({ service: name })({}).then((config) => {
-      NODE.unproxy({ service: name });
+      const name = action.name;
+      const schema = action.schema;
+      
       let json =
       {
-        message0: `Configure ${name.substr(0, name.length - 7)}`,
+        message0: `call ${name}`,
         previousStatement: null,
         nextStatement: null,
+        inputsInline: true,
         colour: 90
       };
       let count = 1;
@@ -81,50 +189,18 @@ function buildConfigBlocks(services)
       {
         if (key !== '__return')
         {
-          if (typeof schema[key] === 'object')
+          json[`args${count}`]= [
           {
-            let def = schema[key].indexOf(config[key]);
-            if (def !== -1)
-            {
-              schema[key].splice(def, 1);
-              schema[key].unshift(config[key]);
-            }
-            json[`args${count}`]= [
-            {
-              type: 'field_dropdown',
-              name: key,
-              check: 'String',
-              options: schema[key].map((value) => [ value, value ])
-            }];
-          }
-          else
-          {
-            switch (schema[key])
-            {
-              case 'String':
-              default:
-                json[`args${count}`] = [
-                {
-                  type: 'field_input',
-                  name: key,
-                  text: key in config ? config[key] : ''
-                }];
-                break;
-              case 'Number':
-                json[`args${count}`] = [
-                {
-                  type: 'field_number',
-                  name: key,
-                  value: key in config ? config[key] : 0
-                }];
-                break;
-            }
-          }
-          json[`message${count}`] = `${key} %1`;
+            type: 'input_value',
+            name: key,
+            check: typeof schema[key] !== 'object' ? schema[key] : 'String',
+            align: 'RIGHT'
+          }];
+          json[`message${count}`] = `${count === 1 ? 'with ' : ''}${key} %1`;
           count++;
         }
       }
-  
+      
       Blockly.Blocks[name] =
       {
         init: function()
@@ -133,113 +209,74 @@ function buildConfigBlocks(services)
         }
       }
 
-      configureBlocks.push(`<block type="${name}" />`);
-
-      if (++blocks == services.length)
-      {
-        const idx = TOOLBOX.indexOf('<category name="Configure">');
-        if (idx !== -1)
-        {
-          TOOLBOX[idx + 1] = configureBlocks.join();
-        }
-        WORKSPACE.updateToolbox(TOOLBOX.join());
-      }
+      actionBlocks.push(`<block type="${name}"></block>`);
     });
-  });
-}
-
-function buildActionBlocks(actions)
-{
-  const actionBlocks = [];
-  actions.forEach((action) => {
-
-    const name = action.name;
-    const schema = action.schema;
-    
-    let json =
+    const idx = TOOLBOX.indexOf('<category name="Actions">');
+    if (idx !== -1)
     {
-      message0: `call ${name}`,
-      previousStatement: null,
-      nextStatement: null,
-      inputsInline: true,
-      colour: 90
-    };
-    let count = 1;
-    for (let key in schema)
-    {
-      if (key !== '__return')
-      {
-        json[`args${count}`]= [
-        {
-          type: 'input_value',
-          name: key,
-          check: typeof schema[key] !== 'object' ? schema[key] : 'String',
-          align: 'RIGHT'
-        }];
-        json[`message${count}`] = `${count === 1 ? 'with ' : ''}${key} %1`;
-        count++;
-      }
+      TOOLBOX[idx + 1] = actionBlocks.join();
     }
-    
-    Blockly.Blocks[name] =
-    {
-      init: function()
-      {
-        this.jsonInit(json);
-      }
-    }
-
-    actionBlocks.push(`<block type="${name}"></block>`);
-  });
-  const idx = TOOLBOX.indexOf('<category name="Actions">');
-  if (idx !== -1)
-  {
-    TOOLBOX[idx + 1] = actionBlocks.join();
+    WORKSPACE.updateToolbox(TOOLBOX.join());
   }
-  WORKSPACE.updateToolbox(TOOLBOX.join());
-}
 
-function buildEventBlocks(events)
-{
-  const eventBlocks = [];
-  events.forEach((event) => {
+  function buildEventBlocks(events)
+  {
+    const eventBlocks = [];
+    events.forEach((event) => {
 
-    Blockly.Blocks[event.name] =
-    {
-      init: function()
+      Blockly.Blocks[event.name] =
       {
-        this.jsonInit(
+        init: function()
         {
-          message0: `%1 of ${event.name}`,
-          args0:
-          [
-            {
-              type: 'field_dropdown',
-              name: 'property',
-              options: Object.keys(event.schema).map((key) => [ key, key ])
-            }
-          ],
-          output: null,
-          colour: 120
-        });
-      }
-    };
-    eventBlocks.push(`<block type="${event.name}"></block>`);
-  });
+          this.jsonInit(
+          {
+            message0: `%1 of ${event.name}`,
+            args0:
+            [
+              {
+                type: 'field_dropdown',
+                name: 'property',
+                options: Object.keys(event.schema).map((key) => [ key, key ])
+              }
+            ],
+            output: null,
+            colour: 120
+          });
+        }
+      };
+      eventBlocks.push(`<block type="${event.name}"></block>`);
+    });
 
-  const idx = TOOLBOX.indexOf('<category name="Events">');
-  if (idx !== -1)
-  {
-    TOOLBOX[idx + 1] = eventBlocks.join();
+    const idx = TOOLBOX.indexOf('<category name="Events">');
+    if (idx !== -1)
+    {
+      TOOLBOX[idx + 1] = eventBlocks.join();
+    }
+    WORKSPACE.updateToolbox(TOOLBOX.join());
   }
-  WORKSPACE.updateToolbox(TOOLBOX.join());
-}
 
-const LIST = NODE.proxy({ service: '/list' });
-LIST({}).then((list) => {
-  NODE.unproxy({ service: '/list' });
-  buildProgramBlocks();
-  buildConfigBlocks(list.services.filter((service) => service.name.endsWith('/config') && service.schema ));
-  buildEventBlocks(list.topics.filter((topic) => topic.schema ));
-  buildActionBlocks(list.services.filter((service) => !service.name.endsWith('/config') && service.schema ));
-});
+  function sort(arr)
+  {
+    arr.sort((a, b) => {
+      return a.name > b.name;
+    });
+    return arr;
+  }
+
+  const LIST = NODE.proxy({ service: '/list' });
+
+  function rebuildEventAndActionBlocks()
+  {
+    LIST({}).then((list) => {
+      buildEventBlocks(sort(list.topics.filter((topic) => topic.schema )));
+      buildActionBlocks(sort(list.services.filter((service) => !service.name.endsWith('/config') && service.schema )));
+    });
+  }
+
+  LIST({}).then((list) => {
+    buildProgramBlocks();
+    buildConfigBlocks(sort(list.services.filter((service) => service.name.endsWith('/config') && service.schema )));
+    buildEventBlocks(sort(list.topics.filter((topic) => topic.schema )));
+    buildActionBlocks(sort(list.services.filter((service) => !service.name.endsWith('/config') && service.schema )));
+  });
+ })();
