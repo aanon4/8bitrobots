c67df700d383ce618011e878b8209561fbcfaaf1
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-04-30 22:43:48 -0700

    Start of app generation infrastructure

---
+++ modules/blockly/8bitapp.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+  function deploy(workspace)
+  {
+    const workspaceText = Block.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+
+    Blockly.JavaScript.INFINITE_LOOP_TRAP = 'if (App.hasTerminated()) { throw new Error("Terminated"); }';
+    const jscode = Blockly.JavaScript.workspaceToCode(workspace);
+  
+    const CONFIG = NODE.proxy({ service: '/app/config' });
+    CONFIG({ workspace: workspaceText, code: jscode }).then(() => {
+      NODE.unproxy({ service: '/app/config' });
+    });
+  }
+
+  window.deployRobotWorkspace = deploy;
+});
