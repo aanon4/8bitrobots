71da2560a9f83ee1e50f3cf2f6194754e2b11681
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-05-08 22:02:00 -0700

    Better tank wheels

--- modules/parts/tank.js
+++ modules/parts/tank.js
@@ -1,32 +1,20 @@
-const PI2 = Math.PI / 2;
-
 function tank(args)
 {
-  const strafe = Math.min(Math.max(args.x, -1), 1);
-  const forward = Math.min(Math.max(args.y, -1), 1);;
-  const output = args.output;
+  let strafe = Math.min(Math.max(args.x, -1), 1);
+  let forward = Math.min(Math.max(args.y, -1), 1);;
 
-  // No movement in any direction, so no output movement in any direction
-  if (forward === 0 && strafe === 0)
-  {
-    return 0;
-  }
-  else
-  {
-    const velocity = (forward < 0 ? -1 : 1) * (forward * forward + strafe * strafe);
-    const angle = Math.abs(Math.atan2(forward, strafe));
-    const wheel = { left: 1, right: 1 };
-    if (angle < PI2)
-    {
-      wheel.left = 1.2 * angle / PI2 - 0.2;
-    }
-    else if (angle > PI2)
-    {
-      wheel.right = -1.2 * angle / PI2 + 2.2;
-    }
+  // Make these ramp up for better slow speed control
+  forward = (forward < 0 ? -1 : 1) * (forward * forward);
+  strafe = (strafe < 0 ? -1 : 1) * (strafe * strafe);
 
-    return Math.min(Math.max(velocity * wheel[output], -1), 1);
+  // Assume left and right wheels rotate cw and ccw
+  // Note: do not reverse the strafe.
+  if (args.output === 'right')
+  {
+    forward = -forward;
   }
+
+  return Math.min(Math.max(forward + strafe * 0.75, -1), 1);
 }
 
 module.exports = tank;
