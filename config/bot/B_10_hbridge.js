95a6e0fde4e77e75a66d42dca02a628fcdf3d953
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2018-05-31 14:52:40 -0700

    Fix name

--- config/bot/B_10_hbridge.js
+++ config/bot/B_10_hbridge.js
@@ -4,7 +4,7 @@ if (typeof PWM !== 'undefined')
 {
   global.HBRIDGE = new HBridge(
   {
-    name: '/i2c/3/66/pwm/hbridge/node',
+    name: '/i2c/3/66/pwm/hbridge',
     ain1: PWM.open({ channel: 15 }),
     ain2: PWM.open({ channel: 14 }),
     bin1: PWM.open({ channel: 13 }),
